cac5738ef189332544656307309648e8c2049fe3
GAJANAN270395 <108356887+GAJANAN270395@users.noreply.github.com>
2023-02-02 18:48:25 +0530

    1 file modified

--- src/pages/products/products.js
+++ src/pages/products/products.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import {useEffect, useState} from 'react';
+import { FakeStoreApi } from '../../services/fake-store-api'
+import { Item } from "../../components/item"
 
 const Products =()=>{
   return(
