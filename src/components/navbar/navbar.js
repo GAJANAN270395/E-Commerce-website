3a4f01fd6799fe72121cf3713fdda17a8d4f0bf1
GAJANAN270395 <108356887+GAJANAN270395@users.noreply.github.com>
2023-02-02 18:26:59 +0530

    2 files modified, 1 added, 1 deleted

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,9 +1,39 @@
-import React from 'react';
-const NavBar = () => {
+import React,{useState} from 'react';
+import { Link } from 'react-router-dom';
+const NavBar = ({onSearch, cartItemCount}) => {
+ 
+  const [searchQuery, setSearchQuery] = useState('')
+  const handleSubmit = ()=>{
+      if(searchQuery.trim().length){
+        onSearch(searchQuery.trim())
+      }
+      setSearchQuery('')
+  }
+
   return (
-    <>
-      <div>Navbar goes here </div>
-    </>
-  );
+  <div className="wrapper">
+      <header className="container">
+        <div className="header py-2">
+          <div className="grid">
+            <Link to="/" className="link">
+              <h1 className="brand">E-Commerce</h1>
+            </Link>
+            <div className="formContainer">
+              <form className="search">
+                <div className="form-control">
+                  <input type="text" value={searchQuery}
+                  onChange ={e=>setSearchQuery(e.target.value)} placeholder="Search products"/>
+                </div>
+                <button type="button" className="search-btn" onClick={handleSubmit}>Search</button>
+              </form>
+            </div>
+            <Link to="/cart" className="link headerCart">
+              <img className="cartImg" src="https://st.depositphotos.com/1835807/1475/i/600/depositphotos_14759785-stock-photo-shopping-cart.jpg" alt="cart"/>
+            </Link>
+          </div>
+          </div>
+      </header>
+  </div>
+  )
 };
 export { NavBar };
